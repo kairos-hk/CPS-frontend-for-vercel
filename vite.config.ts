55042fa191b079485db6d7a96449552e3201eff6
DongyoungKim <kairos.hk0912@gmail.com>
2023-09-05 11:50:44 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://54.180.126.168:5001',
+        changeOrigin: true,
+        secure: false,
+        ws: true
+      }
+    }
+  }
 })
